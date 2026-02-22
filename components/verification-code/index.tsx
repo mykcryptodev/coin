84b79caab3f012dd4fa022350db5d28fadedbb2d
Mike Solomon <mike.solomon@coinbase.com>
2026-02-22 17:22:36 -0500

    feat(verification-code): implement AnimatedCodeNumber and useAnimatedShake (TDD GREEN)

--- components/verification-code/index.tsx
+++ components/verification-code/index.tsx
@@ -1,1 +1,5 @@
-export {};
+export { useAnimatedShake } from './hooks/use-animated-shake';
+export { AnimatedCodeNumber } from './AnimatedCodeNumber';
+export { VerificationCode } from './VerificationCode';
+export type { StatusType } from './AnimatedCodeNumber';
+export type { VerificationCodeRef } from './VerificationCode';
